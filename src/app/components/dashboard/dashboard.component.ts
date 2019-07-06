95731d4c94b1c592417acefdccbf22b6dad5d5c0
Fatih <fatihakgoze@designedbysherpa.com>
2019-07-06 23:27:42 +0300

    Added user list to the dashboard.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -9,12 +9,29 @@ import { Observable } from 'rxjs';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  user: User;
+  users:Observable<User[]>;
+  userDetailShow: boolean = false;
+
+  user_joined_tests:object;
   constructor(
     private _userService: UserService
   ) { }
 
   ngOnInit() {
+    this.users = this._userService.userList();
+    console.log(this.users);
   }
 
+  displayUserDetail(userID: string) {
+    this._userService.userDetails(userID).subscribe(response => {
+      this.user = response;
+      console.log(this.user);
+
+      this.userDetailShow = true;
+    }, error => {
+      this.userDetailShow = false;
+    });
+  }
 
 }
