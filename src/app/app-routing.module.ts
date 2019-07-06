960709ad0e5036abe45cb440761a9af5a68e9e05
Fatih <fatihakgoze@designedbysherpa.com>
2019-07-06 23:40:06 +0300

    Updated routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AddUserComponent } from './add-user/add-user.component';
-import { LoginComponent } from './login/login.component';
-
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'new-user', component: AddUserComponent },
-  { path: 'login', component: LoginComponent } 
+  { path: 'login', component: LoginComponent },
+  { path: 'user-detail/:id', component: UserDetailComponent }
 ];
 
 @NgModule({
